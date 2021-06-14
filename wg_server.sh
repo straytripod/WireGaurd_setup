de4f1d3e9e6a50c546e795b2cfb07904f1d79dde
straytripod <32314078+straytripod@users.noreply.github.com>
2021-06-14 16:31:09 -0600

    Update wg_server.sh

--- wg_server.sh
+++ wg_server.sh
@@ -1,7 +1,9 @@
-apt update && sudo apt upgrade
-apt install wireguard
+apt update && sudo apt upgrade -yy
+apt install wireguard -yy
 umask 077; wg genkey | tee /etc/wireguard/server-privatekey | wg pubkey > /etc/wireguard/server-publickey
 echo "Here is the private key:"
 cat /etc/wireguard/server-privatekey
 echo "Here is the public key:"
+cat /etc/wireguard/server-publickey
+apt install openresolv -yy
 # Need to create the config file
