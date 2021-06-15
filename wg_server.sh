32d613c1e322748365b830786d2a7979b0d8eb84
straytripod <32314078+straytripod@users.noreply.github.com>
2021-06-15 13:14:05 -0600

    Update wg_server.sh

--- wg_server.sh
+++ wg_server.sh
@@ -7,5 +7,28 @@ echo "Here is the public key:"
 cat /etc/wireguard/server-publickey
 apt install openresolv -yy
 # Need to create the config file
-cp ./wg0.conf /etc/wiregaurd/wg0.conf
+cp ./wg0.conf /etc/wiregaurd/wg0.conf# NAT table rules
 chmod 600 /etc/wireguard/wg0.conf
+## Enable IP Forwarding 
+Echo "Enabling IP Forwarding"
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+Echo "Enabling Firewall with port 22 allowed"
+ufw allow 22/tcp
+ufw enable
+echo "Here are the interface names."
+ip -o link show | awk -F'2: ' '{print $2}'
+echo "The second one listed is typcally the main netowrk interface used."
+echo "This interface will be assigned to IP Tables for NAT Filtering."
+ip -o link show | awk -F'2: ' '{print $2}' | awk -F': ' '{print $1}'
+echo "If this is not correct, Press ctrl + c to cancel"
+pause
+NETIF=$(ip -o link show | awk -F'2: ' '{print $2}' | awk -F': ' '{print $1}')
+echo "# NAT table rules" >> /etc/ufw/before.rules
+echo "*nat" >> /etc/ufw/before.rules
+echo ":POSTROUTING ACCEPT [0:0]" >> /etc/ufw/before.rules
+echo "-A POSTROUTING -o "$NETIF" -j MASQUERADE" >> /etc/ufw/before.rules
+echo "" >> /etc/ufw/before.rules
+echo "# End each table with the 'COMMIT' line or these rules won't be processed" >> /etc/ufw/before.rules
+echo "COMMIT" >> /etc/ufw/before.rules
+
